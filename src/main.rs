9e8a74aa83086bacd2616bd75db6235365c4dbed
Felipe Brum <fbrump@gmail.com>
2025-11-26 23:14:06 +0000

    feat: Add list of objects

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,31 @@ struct TransactionDto {
     value: f32,
 }
 
+fn handler_get_transactions() -> Vec<TransactionDto> {
+    let mut transactions: Vec<TransactionDto> = vec![];
+    transactions.push(TransactionDto {
+        name: "Coke".to_string(),
+        value: 10.00,
+    });
+    transactions.push(TransactionDto {
+        name: "Coffee".to_string(),
+        value: 0.30,
+    });
+    transactions.push(TransactionDto {
+        name: "Cookie".to_string(),
+        value: 2.00,
+    });
+    transactions
+}
+
 #[tokio::main]
 async fn main() {
     let root_api = warp::path::end().map(|| "Root");
     // let get_transactions_all = warp::path!("api" / "transactions").map(|| "Transactions");
     let get_transactions_all = warp::get()
         .and(warp::path!("api" / "transactions"))
-        .map(|| {
-            let transaction = TransactionDto {
-                name: "Coke".to_string(),
-                value: 10.00,
-            };
-            warp::reply::json(&transaction)
-        });
+        .map(|| warp::reply::json(&handler_get_transactions()));
+    
     let get_transactions_by_id =
         warp::path!("api" / "transactions" / u32).map(|id: u32| format!("Transactions {}", id));
 

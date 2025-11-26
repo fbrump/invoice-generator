00a9e0bde40fa690d1ccffed138ed6fc36646d3c
Felipe Brum <fbrump@gmail.com>
2025-11-26 23:00:50 +0000

    feat: Improve the get transactions to return JSON

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
+use serde::{Deserialize, Serialize};
+
 use warp::Filter;
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TransactionDto {
+    name: String,
+    value: f32,
+}
 
 #[tokio::main]
 async fn main() {
     let root_api = warp::path::end().map(|| "Root");
-    let get_transactions_all = warp::path!("api" / "transactions").map(|| "Transactions");
+    // let get_transactions_all = warp::path!("api" / "transactions").map(|| "Transactions");
+    let get_transactions_all = warp::get()
+        .and(warp::path!("api" / "transactions"))
+        .map(|| {
+            let transaction = TransactionDto {
+                name: "Coke".to_string(),
+                value: 10.00,
+            };
+            warp::reply::json(&transaction)
+        });
     let get_transactions_by_id =
         warp::path!("api" / "transactions" / u32).map(|id: u32| format!("Transactions {}", id));
 

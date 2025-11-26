f5d8045acb3f7525dbb4c1aa1e13ce20ef3fd690
Felipe Brum <fbrump@gmail.com>
2025-11-26 21:40:00 +0000

    feat: Another try on this framework

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::{convert::Infallible, error::Error, net::SocketAddr};
+use std::{error::Error, net::SocketAddr};
 
 use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::body::Buf;
 use hyper::{
     body::Bytes, server::conn::http1, service::service_fn, Method, Request, Response, StatusCode,
 };
+use hyper::{body::Incoming as IncomingBody, header};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
@@ -19,9 +21,20 @@ fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
         .boxed()
 }
 
-async fn get_transactions() -> Result<Response<Full<Bytes>>, Infallible> {
-    let response_body = Full::new(Bytes::from("Transactions"));
-    Ok(Response::new(response_body))
+async fn api_post_response(req: Request<IncomingBody>) -> Result<Response<BoxBody>> {
+    // Aggregate the body...
+    let whole_body = req.collect().await?.aggregate();
+    // Decode as JSON...
+    let mut data: serde_json::Value = serde_json::from_reader(whole_body.reader())?;
+    // Change the JSON...
+    data["test"] = serde_json::Value::from("test_value");
+    // And respond with the new JSON.
+    let json = serde_json::to_string(&data)?;
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .header(header::CONTENT_TYPE, "application/json")
+        .body(full(json))?;
+    Ok(response)
 }
 
 async fn router_service(
@@ -29,7 +42,7 @@ async fn router_service(
 ) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/") => Ok(Response::new(full("Welcome to the Invoice Generating"))),
-        (&Method::GET, "/api/transactions") => get_transactions(),
+        (&Method::GET, "/api/transactions") => get_transactions().await,
 
         // Return 404 Not Found for other routes.
         _ => {
@@ -45,7 +58,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let port = 3000;
     let host = [127, 0, 0, 1];
     let address = SocketAddr::from((host, port));
-    let listner = TcpListener::bind(address).await?;
+    let listner = TcpListener::bind(&address).await?;
 
     loop {
         let (stream, _) = listner.accept().await?;

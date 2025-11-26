0fce87351f419d7c46113cc459c696dcd2d47ce4
Felipe Brum <fbrump@gmail.com>
2025-11-26 22:37:53 +0000

    feat: Apply basic usage of the WARP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Welcome to Invoice Generator!");
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    let root_api = warp::path::end().map(|| "Root");
+    let get_transactions_all = warp::path!("api" / "transactions").map(|| "Transactions");
+    let get_transactions_by_id =
+        warp::path!("api" / "transactions" / u32).map(|id: u32| format!("Transactions {}", id));
+
+    let routers = warp::get().and(root_api.or(get_transactions_all).or(get_transactions_by_id));
+
+    warp::serve(routers).run(([127, 0, 0, 1], 3030)).await;
 }

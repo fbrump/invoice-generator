8b8e9a904cea466d18d3d8c0a853e5f3ec839022
Felipe Brum <fbrump@gmail.com>
2025-11-26 01:10:42 +0000

    feat: Setup the server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Welcome to Invoice Generator!");
+use std::{convert::Infallible, error::Error, net::SocketAddr};
+
+use http_body_util::Full;
+use hyper::{body::Bytes, server::conn::http1, service::service_fn, Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let port = 3000;
+    let host = [127, 0, 0, 1];
+    let address = SocketAddr::from((host, port));
+    let listner = TcpListener::bind(address).await?;
+
+    loop {
+        let (stream, _) = listner.accept().await?;
+        let io = TokioIo::new(stream);
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                eprintln!(" Error serving connection: {:?}", err);
+            }
+        });
+    }
 }

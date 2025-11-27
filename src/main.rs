2cad4e09e9dcdcccb877e685a4f689c5dc3b5acc
Felipe Brum <fbrump@gmail.com>
2025-11-27 00:30:09 +0000

    feat: Completed to improve the get transaction by ID

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use serde::{Deserialize, Serialize};
 
-use warp::Filter;
+use warp::{reply::Reply, Filter};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, Clone)]
 struct TransactionDto {
     id: u32,
     name: String,
     value: f32,
-    merchant_name: String
+    merchant_name: String,
 }
 
 fn handler_get_transactions() -> Vec<TransactionDto> {
@@ -33,6 +33,14 @@ fn handler_get_transactions() -> Vec<TransactionDto> {
     transactions
 }
 
+fn handler_get_transactions_by_(id: u32) -> Option<TransactionDto> {
+    let result_item = handler_get_transactions().into_iter().find(|i| i.id == id);
+    match result_item {
+        None => None,
+        Some(item) => Some(item),
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let root_api = warp::path::end().map(|| "Root");
@@ -42,7 +50,16 @@ async fn main() {
         .map(|| warp::reply::json(&handler_get_transactions()));
 
     let get_transactions_by_id =
-        warp::path!("api" / "transactions" / u32).map(|id: u32| format!("Transactions {}", id));
+        warp::path!("api" / "transactions" / u32).map(
+            |id: u32| match handler_get_transactions_by_(id) {
+                None => warp::reply::with_status(
+                    "There is not transaction",
+                    warp::http::StatusCode::BAD_REQUEST,
+                )
+                .into_response(),
+                Some(item) => warp::reply::json(&item).into_response(),
+            },
+        );
 
     let routers = warp::get().and(root_api.or(get_transactions_all).or(get_transactions_by_id));
 

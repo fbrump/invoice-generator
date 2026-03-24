6c55d29de62ada6699fc1f3200f16a5672755adf
Felipe Brum <fbrump@gmail.com>
2026-03-24 00:39:47 +0000

    feat: Setup Axum server
    - Configure tracing
    - Configure log
    - Add health endpoint /api/monitoing/health
    
    ticket: #4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Welcome to Invoice Generator!");
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+use tower_http::trace::TraceLayer;
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+#[tokio::main]
+async fn main() {
+    // 1. Initialize tracing + log bridging
+    tracing_subscriber::fmt()
+        // This allows you to use, e.g., `RUST_LOG=info` or `RUST_LOG=debug`
+        // when running the app to set log levels.
+        .with_env_filter(
+            EnvFilter::try_from_default_env()
+                .or_else(|_| EnvFilter::try_new("axum_tracing_example=error,tower_http=warn"))
+                .unwrap(),
+        )
+        .with_file(true)
+        .init();
+
+    // app
+    let router_api = Router::new().route("/monitoring/health", get(|| async { "OK" }));
+    let app = Router::new()
+        .route(
+            "/",
+            get(|| async {
+                info!("Root endpoint was called");
+                "Welcome to Invoice Generator API"
+            }),
+        )
+        .nest("/api", router_api)
+        .layer(TraceLayer::new_for_http());
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    info!("Starting server on {}", addr);
+    // run app
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    // run it
+    let _ = axum::serve(listener, app).await;
 }

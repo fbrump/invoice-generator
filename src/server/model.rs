151d686a72647e0f8982f0ffd242b6fc7aadb0d9
Felipe Brum <fbrump@gmail.com>
2026-03-28 23:29:26 +0000

    feat: Implement Get Transactions by Id
    - Add new endpoint
    - Register router /api/transactions/{id:UUID}
    
    ticket: #6

--- src/server/model.rs
+++ src/server/model.rs
@@ -1,19 +1,19 @@
+use chrono::{DateTime, Utc};
 use serde::Serialize;
 use uuid::Uuid;
-use chrono::{DateTime, Utc};
 
 #[derive(Debug, Clone, Serialize)]
-pub enum CurrencyEnum { 
+pub enum CurrencyEnum {
     EURO,
     DOLLAR,
 }
 
 #[derive(Debug, Clone, Serialize)]
 pub struct Transaction {
-    pub id : Uuid,
+    pub id: Uuid,
     pub description: String,
     pub merchant_name: String,
     pub value: f32,
     pub currency: CurrencyEnum,
-    pub date_time: DateTime<Utc>, 
+    pub date_time: DateTime<Utc>,
 }
